34e991ecd38bbfdbadbf29b702263fb22047bc8e
Sam Rossin <sam.rossin@gmail.com>
2015-06-15 14:41:52 -0700

    Main Activity OnClickListener

--- app/src/main/java/com/rossin/sam/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/rossin/sam/spotifystreamer/MainActivityFragment.java
@@ -1,10 +1,13 @@
 package com.rossin.sam.spotifystreamer;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -44,6 +47,17 @@ public class MainActivityFragment extends Fragment {
 
         //bind results
         listView.setAdapter(mArtistAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,View view,
+                                    int position, long l){
+                String artist = mArtistAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), SongsActivity.class).
+                        putExtra(Intent.EXTRA_TEXT, artist);
+                startActivity(intent);
+            }
+        });
+
         return rootView;
     }
 }
--- app/src/main/java/com/rossin/sam/spotifystreamer/SongsActivityFragment.java
+++ app/src/main/java/com/rossin/sam/spotifystreamer/SongsActivityFragment.java
@@ -1,7 +1,9 @@
 package com.rossin.sam.spotifystreamer;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,14 +28,16 @@ public class SongsActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_songs, container, false);
-
         //fake data
-        String[] temp_data = {"Jon doe", "bob", "joseph", "Catfish", "wow", "more crap"
-                ,"some stuff", "a guy"};
+        String[] temp_data = new String[10];
+        String name = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        for(int i=0; i<10; i++) temp_data[i] = name;
+
+
         List<String> song = new ArrayList<String>(Arrays.asList(temp_data));
 
         mSongsAdapter = new ArrayAdapter<String>(getActivity(),
-                R.id.list_item_song, R.id.text_view_song, song);
+                R.layout.list_item_song, R.id.text_view_song, song);
 
         //get reference to listview
         ListView listView = (ListView) rootView.findViewById(R.id.list_view_songs_results);
