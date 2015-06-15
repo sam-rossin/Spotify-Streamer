84ae9cf0b711634491def0f0dab91fd10fdc5129
Sam Rossin <sam.rossin@gmail.com>
2015-06-15 13:27:03 -0700

    Add Spotify Api Library

--- app/src/main/java/com/rossin/sam/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/rossin/sam/spotifystreamer/MainActivityFragment.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    ArrayAdapter<String> mArtistAdapter;
 
     public MainActivityFragment() {
     }
@@ -18,6 +25,26 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        //inflate layout
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //create the adapter to fill list view
+
+        //fake data
+        String[] temp_data = {"Jon doe", "bob", "joseph", "Catfish", "wow", "more crap"
+                ,"some stuff", "a guy"};
+        List<String> list = new ArrayList<String>(Arrays.asList(temp_data));
+
+        mArtistAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_artist, R.id.textview_artist_name, list);
+
+
+        //get reference to listview
+        ListView listView = (ListView) rootView.findViewById(R.id.list_view_artist_results);
+
+        //bind results
+        listView.setAdapter(mArtistAdapter);
+
+        return rootView;
     }
 }
